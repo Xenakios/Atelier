b9e8e4f059c5e1bc21da6c584c87c97369e24d34
Xenakios <xenakios@gmail.com>
2020-07-26 22:47:56 +0300

    Hopefully fixed the bug where the engine LED widget might change the engine when manipulating other widgets in the panel and releasing the mouse button

--- src/Palette.cpp
+++ src/Palette.cpp
@@ -583,6 +583,8 @@ struct Model_LEDWidget : public TransparentWidget
 	}
 	void onButton(const event::Button& e) override
 	{
+		if (e.action!=GLFW_PRESS)
+			return;
 		int engineBank = mPalette->patch[0].engine / 8;
 		for (int i=0;i<8;++i)
 		{
